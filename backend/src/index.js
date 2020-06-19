d7f9bff7cec824c547a57e7c97965c552f4b92b1
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-18 21:23:54 -0300

    3.1 Métodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,47 @@
 const express = require('express'); //REALIZAR A IMPORTACAO DE UMA BIBLIOTECA
+const { response } = require('express');
 
 const app = express(); //CRIA UMA VARIAVEL PARA PODER UTILIZAR AS FERRAMENTAS DO EXPRESS
 
 //DEFINE AS ROTAS DO PROGRAMA
 app.get('/', (request, response) => { //GET = CONSULTAR
-  return response.json({ message: 'Hello Word - Inicio' }); //ENVIA UMA MENSAGEM EM JSON
+  return response.json(['Hello Word']); //ENVIA UMA MENSAGEM EM JSON
 });
 
+//O QUE VEM DEPOIS DA BARRA CHAMAMOS DE RECURSO
 app.get('/projetos', (request, response) => {
-  return response.send('Hello Word - Projetos'); //ENVIA UMA MENSAGEM EM SEND
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]); //ENVIA UMA MENSAGEM EM SEND
+});
+
+app.post('/projetos', (request, response) => { //POST = CRIAR
+   return response.json([
+     'Projeto 1',
+     'Projeto 2',
+     'Projeto 3',
+   ]);
+});
+
+//O : DEFINE QUE DEVEMOS RECEBER UM PARAMETRO
+app.put('/projetos/:id', (request, response) => { //PUT = ALTERAR
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Proejto 3',
+  ]);
+});
+
+app.delete('/projetos/:id', (request, response) => { //DELETE = DELETAR
+  return response.json([
+    'Projeto 4',
+    'Projeto 3',
+  ]);
 });
 
 //DEFINE UMA PORTA PARA EXECUCAO POIS O CAMINHO SEMPRE É localhost
 //listen PODE RECEBER UMA FUNCAO ASSIM PODENDO CRIAR MAIS COMANDOS
 app.listen(3333, () => {
-  console.log('SERVIDOR EXECUTANDO 😎🚀'); //A FUNCAO EXECUTA ESTA MENSAGEM
+  console.log('SERVIDOR EXECUTANDO 😎'); //A FUNCAO EXECUTA ESTA MENSAGEM
 });
