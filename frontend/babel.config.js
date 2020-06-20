b966511711349b1f4ef0609bc2f700570ef25615
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-20 14:36:16 -0300

    4.1 Configurando Babel

---
+++ frontend/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    //CONVERTE O JAVACRIPT PARA UMA VERSAO COMPATIVEL COM O AMBIENTE DE TRABALHO
+    '@babel/preset-env',
+     //ADICIONA AS FUNCIONALIDADES BASEANDO NA VERSAO DO AMBIENTE DE TRABALHO
+    '@babel/preset-react',
+  ],
+};
