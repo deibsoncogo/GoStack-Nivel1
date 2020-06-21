4c0318f0768a72abb92366c3868e12d8b54cba35
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-20 21:00:04 -0300

    4.1 Estado e Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 //COLOCAR A PRIMEIRA LETRA EM MAIUSCULA PARA COMPONENTES
 //APP E UM COMPONENTE PRINCIPAL, OS DEMIAS DEVEM SER CRIADOS NA PASTA
 
-import React from 'react'; //SEMPRE REALIZAR ESTA IMPORTACAO
+import React, { useState } from 'react'; //SEMPRE REALIZAR ESTA IMPORTACAO
 
 import CabecalhoComponente from './components/Cabecalho';
 
 function App() { //FUNCAO COMPONENTE
+  //TRANSFORMAR UMA VARIAVEL COM ESTADO BASTA UTILIZAR O COMANDO: useState
+  //ELA CRIA UM ARRAY COM DOIS CAMPOS DA CADA INFORMACAO
+  //NO PRIMEIRO SERA O VALOR ATUAL E NO OUTRO O NOVO
+  //AO VINCULA COM O BACKEND ESTE METODO NAO FUNCIONA MAIS POIS DEVE INICIAR EM BRANCO
+  const [projetos, setProjetos] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  // handle NOS AVISA QUE ESTA FUNCAO E EXECUTADA PELO USUARIO
+  function handleAdicionarProjeto() {
+    //ESCREVER DENTRO DE `` PERMITE MESCLAR TEXTO COM VARIAVEIS
+    // projetos.push(`Novo projeto ${ Date.now() }`);
+    setProjetos([...projetos, `Novo projeto ${ Date.now() }`]);
+  }
+
   return( //PARA UTILIZAR MAIS DE UM COMANDO PRECISAR CRIAR UM "GRUPO" FRAGMENT OU NAO
     <> {/* INICIO DO METODO FRAGMENT */}
       <h1>Hello World</h1>
@@ -16,11 +29,13 @@ function App() { //FUNCAO COMPONENTE
           <li>React Native</li>
         </ul>
       </CabecalhoComponente>
-      <CabecalhoComponente titulo='Linguagem'>
+      <CabecalhoComponente titulo='Projetos'>
         <ul>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
+          {/* key DEFINE UM CODIGO UNICO PARA O ITEN */}
+          {projetos.map(projetos => <li key={projetos}>{projetos}</li>)}
         </ul>
+
+        <button type="button" onClick={handleAdicionarProjeto}>Adicionar projeto</button>
       </CabecalhoComponente>
     </> /* TERMINO DO METODO FRAGMENT */
   );
