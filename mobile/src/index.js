0c61e5bc8fa11fd20b665db88981247299778dc9
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-25 20:21:24 -0300

    5.1 Diferenças do ReactJS

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      {/* DEFINE A COR DOS ICONE E DE FUNDO NO TOPO */}
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1'/>
+      {/* VINCULA O ESTILO NO View */}
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Hello Word</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { //PODEMOS USAR QUALQUER NOME
+    flex: 1, //ATIVA O FLEX PARA 1
+    backgroundColor: '#7159C1', //COR DE FUNDO
+    justifyContent: 'center', //CENTRALIZAR VERTICALMENTE
+    alignItems: 'center', //CENTRALIZAR HORIZONTALMENTE
+  },
+
+  titulo: {
+    color: '#ffffff', //COR DO TEXTO
+    fontSize: 32, //TAMANHO DO TEXTO
+    fontWeight: 'bold', //DEFINE COMO NEGRITO
+  },
+});
